f80887d4d1fccb40c228efe188d8f201223e95ee
hanif <shaikhanif2004@gmail.com>
2026-09-04 12:22:47 +0530

    Created and Tested matches routes

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 import express from "express";
+import { matchRouter } from "./routes/matches.js";
 
 const app = express();
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (_, res) => {
   res.send(`<h1>Sportz API Server</h1>`);
 });
+
+app.use("/api/matches", matchRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
